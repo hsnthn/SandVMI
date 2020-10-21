64793f5e4406078712cfa421e7b6af4be593c79c
Nick <nikitavaskow@gmail.com>
2020-10-21 12:04:49 +0300

    Integrated swclient

--- app/src/main/java/com/cybercrypt/sandvmi/api/ApiConstants.java
+++ app/src/main/java/com/cybercrypt/sandvmi/api/ApiConstants.java
@@ -2,7 +2,7 @@ package com.cybercrypt.sandvmi.api;
 
 
 public class ApiConstants {
-    public static final String BASE =  "http://185.165.169.244:8080/";
+    public static final String BASE =  "http://88.119.23.19/";
     public static final String LOGIN =  "login";
     public static final String SIGNUP =  "signup";
 }
--- app/src/main/java/com/cybercrypt/sandvmi/ui/authentication/login/LoginViewModel.java
+++ app/src/main/java/com/cybercrypt/sandvmi/ui/authentication/login/LoginViewModel.java
@@ -10,6 +10,16 @@ import com.cybercrypt.sandvmi.api.Resources;
 import com.cybercrypt.sandvmi.data.remote.model.User;
 import com.cybercrypt.sandvmi.data.remote.response.LoginResponse;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import retrofit2.Response;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -19,9 +29,10 @@ public class LoginViewModel extends ViewModel {
     MutableLiveData<Resources> mutableLiveData = new MutableLiveData<>();
 
     void getUser(final String username, final String password) {
+        trustAllCertificates();
         Client.getInstance()
                 .getService()
-                .login(username,password)
+                .login(username, password)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<Response<LoginResponse>>() {
@@ -37,14 +48,46 @@ public class LoginViewModel extends ViewModel {
 
                     @Override
                     public void onNext(Response<LoginResponse> response) {
-                        Log.w("sonuc",response.toString());
+                        Log.w("sonuc", response.toString());
                         if (response.code() == 200) {
                             User user = new User();
-                            user.username=username;
-                            mutableLiveData.setValue(Resources.success(user,""));
-                        }else
-                            mutableLiveData.setValue(Resources.error("",null));
+                            user.username = username;
+                            mutableLiveData.setValue(Resources.success(user, ""));
+                        } else
+                            mutableLiveData.setValue(Resources.error("", null));
                     }
                 });
+    }
+
+    public void trustAllCertificates() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+                            return myTrustedAnchors;
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+        }
     }
 }
